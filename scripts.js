1b8c8ccd7084df9b4a8828c8da66f4eacc984e24
Alejandro Rodriguez <107013247+FuturaKiosk@users.noreply.github.com>
2022-06-22 13:33:34 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const counterEl = document.getElementById('counter')
+const totalCount = document.getElementById('total')
+let count = 0
+
+function increase() {
+  // count = count + 1 
+  count += 1 // this shorthand for the line before
+  counterEl.innerHTML = count
+}
+
+function decrease(){
+  // count = count - 1
+  count -= 1 // this shorthand for the line before
+  counterEl.innerText = count
+}
+
+function printTotal(){
+  let countStr = count + ' - '
+  totalCount.innerHTML += countStr
+}
